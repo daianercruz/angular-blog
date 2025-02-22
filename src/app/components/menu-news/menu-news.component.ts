2460ffd95aec31b2d658e32abc4105fd0ab3bca3
Daiane Cruz <daianercruz12@gmail.com>
2025-02-22 10:59:19 -0300

    feat: styled title

---
+++ src/app/components/menu-news/menu-news.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-news',
+  imports: [],
+  templateUrl: './menu-news.component.html',
+  styleUrl: './menu-news.component.css'
+})
+export class MenuNewsComponent {
+
+}
